27a0b5e1f9b4051fa61389fb9f7c429c0925f04a
maslopiotr <maslopiotr@gmail.com>
2022-06-27 21:22:09 +0100

    sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 module.exports = {
   // see https://github.com/iamvishnusankar/next-sitemap
-  siteUrl: 'https://next.jamify.org',
+  siteUrl: 'https://strefatest.netlify.app',
   generateRobotsTxt: true,
   sitemapSize: 7000,
 }
